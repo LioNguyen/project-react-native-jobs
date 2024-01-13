2b3ef2a6073cf96053dbdc00dbc2766431b911b3
Nghi Nguyen Thanh <nghi.nguyen@evehr.vn>
2024-01-13 15:05:11 +0700

    feat: create welcome section & popular job list section

--- components/common/cards/popular/PopularJobCard.jsx
+++ components/common/cards/popular/PopularJobCard.jsx
@@ -1,14 +1,16 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import { Text, TouchableOpacity, View } from "react-native";
 
-import styles from './popularjobcard.style'
+import styles from "./popularjobcard.style";
 
-const PopularJobCard = () => {
+const PopularJobCard = ({ item, selectedJob, handleCardPress }) => {
   return (
-    <View>
-      <Text>PopularJobCard</Text>
-    </View>
-  )
-}
+    <TouchableOpacity
+      style={styles.container(selectedJob, item)}
+      onPress={() => handleCardPress(item)}
+    >
+      <Text>{item}</Text>
+    </TouchableOpacity>
+  );
+};
 
-export default PopularJobCard
+export default PopularJobCard;
